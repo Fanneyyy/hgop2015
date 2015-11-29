f1df088b2653ad33279c8852acb8731860c1b7d3
Fanneyyy <fanneyyyy@gmail.com>
2015-11-29 15:00:13 +0000

    Created a deployment script to push the newest docker image from the development environment and pull it into the test environment to make sure that the test environment is running the latest version

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+echo Turning on the Development Environment
+cd ./vagrant
+vagrant up
+
+echo Connecting to Development Environment
+echo Starting docker
+vagrant ssh -c 'sudo service docker start'
+
+echo Login to docker
+vagrant ssh -c 'docker login --username=fanneyyy --password=$DOCKER_PASS --email=$DOCKER_EMAIL'
+
+echo Pushing newest docker image
+vagrant ssh -c 'docker push fanneyyy/tictactoe'
+
 echo Turn on the Test Environment
 cd ../testEnv
 vagrant up
 
 echo Connecting to Test Environment
-vagrant ssh -c 'pwd'
+echo Pulling newest docker image
+vagrant ssh -c 'docker pull fanneyyy/tictactoe'
