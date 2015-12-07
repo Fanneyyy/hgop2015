0c096bbf4f125a569cb549a989b36a4b495ae3ae
Fanneyyy <fanneyyyy@gmail.com>
2015-12-07 13:18:52 +0000

    Added test for returning a stored event, no further logic needed

--- server/event_storage/memory_storage/memoryStorage.spec.js
+++ server/event_storage/memory_storage/memoryStorage.spec.js
@@ -1,15 +1,27 @@
 var should = require('should');
-var inMemoryStore = require('./memoryStorage');
+var inMemoryStorage = require('./memoryStorage');
 
 describe('in memory storage', function() {
 
   it('should return an empty array for an unknown id', function() {
 
-    var localStore = inMemoryStore();
+    var localStorage = inMemoryStorage();
 
-    localStore.loadEvents('1234',function(err, loadedEvents){
+    localStorage.loadEvents('1234',function(err, loadedEvents) {
       should(loadedEvents.length).be.exactly(0);
       should(loadedEvents).be.instanceof(Array);
+    });
+
+  });
+
+  it('should return events previously stored', function() {
+
+    var localStorage = inMemoryStorage();
+
+    localStorage.loadEvents('1234', [{"id":"1"}]).then(function() {
+      localStorage.loadEvents('1234').then(function(loadedEvents) {
+        should(loadedEvents).eql([{"id":"1"}]);
+      });
     });
 
   });
