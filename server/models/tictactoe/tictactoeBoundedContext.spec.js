12ba8f1b6128a172a1942d67293b302cc75d9ede
Fanneyyy <fanneyyyy@gmail.com>
2015-12-07 17:14:04 +0000

    installed mockito for mock testing

--- server/models/tictactoe/tictactoeBoundedContext.spec.js
+++ server/models/tictactoe/tictactoeBoundedContext.spec.js
@@ -54,44 +54,44 @@ describe('tictactoe game context', function() {
     });
   });
 
-  //it('should route command to instantiated tictactoe game with event stream from storage and return generated events, ' +
-  //  'using mock style tests.',function(done) {
-  //
-  //  var jm = require('jsmockito').JsMockito;
-  //  jm.Integration.importTo(global);
-  //  /* global spy,when */
-  //
-  //  var mockStore = spy({
-  //    loadEvents: function() {
-  //      return resolvedPromise([]);
-  //    },
-  //    storeEvents: function(events) {
-  //      return resolvedPromise(events);
-  //    }
-  //  });
-  //
-  //  var mockTickTackToe = spy({
-  //    executeCommand: function() {
-  //      return resolvedPromise([]);
-  //    }
-  //  });
-  //
-  //  var commandHandlers = function() {
-  //    return mockTickTackToe
-  //  };
-  //  var boundedContext = require('./tictactoeBoundedContext')(mockStore, commandHandlers);
-  //
-  //  var emptyCommand = {
-  //    gameId: "123"
-  //  };
-  //
-  //  boundedContext.handleCommand(emptyCommand).then(function() {
-  //    jm.verify(mockStore).loadEvents('123');
-  //    jm.verify(mockStore).storeEvents('123');
-  //
-  //    jm.verify(mockTickTackToe).executeCommand(emptyCommand);
-  //
-  //    done();
-  //  });
-  //});
+  it('should route command to instantiated tictactoe game with event stream from storage and return generated events, ' +
+    'using mock style tests.',function(done) {
+
+    var jm = require('jsmockito').JsMockito;
+    jm.Integration.importTo(global);
+    /* global spy,when */
+
+    var mockStore = spy({
+      loadEvents: function() {
+        return resolvedPromise([]);
+      },
+      storeEvents: function(events) {
+        return resolvedPromise(events);
+      }
+    });
+
+    var mockTickTackToe = spy({
+      executeCommand: function() {
+        return resolvedPromise([]);
+      }
+    });
+
+    var commandHandlers = function() {
+      return mockTickTackToe
+    };
+    var boundedContext = require('./tictactoeBoundedContext')(mockStore, commandHandlers);
+
+    var emptyCommand = {
+      gameId: "123"
+    };
+
+    boundedContext.handleCommand(emptyCommand).then(function() {
+      jm.verify(mockStore).loadEvents('123');
+      jm.verify(mockStore).storeEvents('123');
+
+      jm.verify(mockTickTackToe).executeCommand(emptyCommand);
+
+      done();
+    });
+  });
 });
