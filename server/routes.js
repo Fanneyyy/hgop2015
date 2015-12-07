f37a248eedb6087d9a6e1a19862fcd7656f0e093
Fanneyyy <fanneyyyy@gmail.com>
2015-12-07 14:19:34 +0000

    Changed the main app file to send the environment config when getting route info, and route to recieve it to find the event/memory storage

--- server/routes.js
+++ server/routes.js
@@ -6,11 +6,18 @@
 
 var errors = require('./components/errors');
 
-module.exports = function(app) {
+module.exports = function(app, config) {
+
+  var eventStorage = require('.' + config.event_storage)();
 
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
-  
+  app.get("/api/testApi", function(req, res){
+    res.json({"test":"test"});
+  });
+
+  app.use('/api/gameHistory', require('./api/gameHistory')(eventStorage).router);
+  app.use('/api/createGame', require('./api/createGame')(eventStorage).router);
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
