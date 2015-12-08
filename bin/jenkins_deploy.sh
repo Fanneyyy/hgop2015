8f86733509fa00eb1f346d5cb21c3f915ba843b8
Fanneyyy <fanneyyyy@gmail.com>
2015-12-08 13:48:00 +0000

    Attempt 2, testing the acceptance test with jenkins

--- bin/jenkins_deploy.sh
+++ bin/jenkins_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 echo 'Jenkins deployment stage script'
+# problems with finding grunt
+export PATH=/usr/local/bin:$PATH;
 
+npm install
+bower install
+
+# because of problems with graphic cards
+export DISPLAY=:0
 ./bin/deploy.sh
