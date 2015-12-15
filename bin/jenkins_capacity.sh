e0250ccdc246668c30f918d4c0a382a7c5fc7e78
Fanneyyy <fanneyyyy@gmail.com>
2015-12-15 20:30:49 +0000

    Refactoring scripts for capacity test stage

--- bin/jenkins_capacity.sh
+++ bin/jenkins_capacity.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PORT=8080
+IP=192.168.50.4
+
 echo 'Jenkins load/capacity test script'
 # problems with finding grunt
 export PATH=/usr/local/bin:$PATH;
@@ -9,6 +12,7 @@ EXITCODE=$?; if [[ $EXITCODE != 0 ]]; then
 	echo "Failure in npm install, exit code: " $EXITCODE
 	exit $EXITCODE;
 fi
+
 bower install
 EXITCODE=$?; if [[ $EXITCODE != 0 ]]; then
 	echo "Failure in bower install, exit code: " $EXITCODE
@@ -17,6 +21,8 @@ fi
 
 # because of problems with graphic cards
 export DISPLAY=:0
-export ACCEPTANCE_URL=http://192.168.50.4:8080
+export ACCEPTANCE_URL=http://$IP:$PORT
 
 grunt mochaTest:load
+
+echo "Done"
