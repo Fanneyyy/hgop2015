270fd97b15e28484dc25d275aad08a0fecf6089b
Fanneyyy <fanneyyyy@gmail.com>
2015-12-15 21:30:04 +0000

    Fixing some issue with the quotes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,17 +3,14 @@
 echo Connecting to Test Environment
 echo Running $1 on port $2 on $3
 
-ssh vagrant@$3 '
+ssh vagrant@$3 "
 	(echo Removing currently running images &&
-	if [ ! -z "tictactoe$2" ]
-	then
 	docker kill tictactoe$2 &&
-	docker rm  tictactoe$2
-	fi &&
+	docker rm  tictactoe$2 &&
 	echo Pulling newest docker image &&
-	docker pull fanneyyy/tictactoe &&
+	docker pull fanneyyy/tictactoe:$1 &&
 	echo Running newest docker image &&
-	docker run -p $2:8080 -d --name tictactoe$2 -e "NODE_ENV=production" fanneyyy/tictactoe:$1)'
+	docker run -p $2:8080 -d --name tictactoe$2 -e NODE_ENV=production fanneyyy/tictactoe:$1)"
 EXITCODE=$?; if [[ $EXITCODE != 0 ]]; then exit $EXITCODE; fi
 
 echo "Done"
